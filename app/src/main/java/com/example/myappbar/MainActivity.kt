366a1613c5c569175dbb831d4d7533d4038aca2d
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2025-02-05 22:50:17 +0700

    add menu navigation

--- app/src/main/java/com/example/myappbar/MainActivity.kt
+++ app/src/main/java/com/example/myappbar/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.myappbar
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myappbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBarr.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu1 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_fragment, MenuFragment()).addToBackStack(null).commit()
+                    true
+                }
+
+                R.id.menu2 -> {
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> false
+
+            }
+        }
     }
 }
